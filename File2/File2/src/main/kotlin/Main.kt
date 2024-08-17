42ab2b09e51e015a8462a7fde90390174fe0cf95
anastasia-zh97 <abaabady-97@yandex.ru>
2024-08-17 18:05:23 +0300

    Second commit

--- File2/File2/src/main/kotlin/Main.kt
+++ File2/File2/src/main/kotlin/Main.kt
@@ -1,61 +1,55 @@
 package ru.Netology
 
 fun main() {
-    val monthLimitForMasterCard = 75000
-    var transaction = 1000
-    val cardType = "Visa"
-
-    countTax(cardType, monthLimitForMasterCard, transaction);
+    var transaction = 20000
+    var allMonthTransactions = 80000
+    countTax(allMonthTransactions, transaction)
 }
 
-fun countTax(card: String, allMonthTransactions: Int, transactionSum: Int): Int {
+fun countTax(allMonthTransactions: Int, transactionSum: Int, card: String = "МИР"): Int {
     var tax = 0
     val dayLimit = 150000
     val monthLimit = 600000
-    var monthExpenses = 0
-    var dayExpenses = 0
+    val masterCardLimit = 75000
     if (card == "МИР") {
-        if (transactionSum < dayLimit && (monthExpenses + transactionSum) < monthLimit &&
-            (dayExpenses + transactionSum) < dayLimit
-        ) {
+        if (transactionSum < dayLimit && (allMonthTransactions + transactionSum) < monthLimit) {
             println("Ваша комиссия состаляет " + tax + " руб")
-            monthExpenses += transactionSum
-            dayExpenses += transactionSum
         } else {
             println("Перевод не может быть произведен, поскольку привышен лимит")
         }
-    } else if (card == "Mastercard" && transactionSum < allMonthTransactions) {
-        if (transactionSum < dayLimit && (monthExpenses + transactionSum) < monthLimit &&
-            (dayExpenses + transactionSum) < dayLimit
-        ) {
+    } else if (card == "Mastercard" && allMonthTransactions > masterCardLimit
+        && (allMonthTransactions + transactionSum) < monthLimit
+    ) {
+        if (transactionSum < dayLimit) {
+            tax = (transactionSum * 0.006).toInt() + 20
             println("Ваша комиссия состаляет " + tax + " руб")
-            monthExpenses += transactionSum
-            dayExpenses += transactionSum
         } else {
             println("Перевод не может быть произведен, поскольку привышен лимит")
         }
-    } else if (card == "Mastercard" && transactionSum > allMonthTransactions) {
-        if (transactionSum < dayLimit && (monthExpenses + transactionSum) < monthLimit &&
-            (dayExpenses + transactionSum) < dayLimit
-        ) {
-            tax = ((transactionSum - allMonthTransactions) * 0.06).toInt() + 20
+    } else if (card == "Mastercard" && (allMonthTransactions + transactionSum) < masterCardLimit
+        && (allMonthTransactions + transactionSum) < monthLimit
+    ) {
+        if (transactionSum < dayLimit) {
             println("Ваша комиссия состаляет " + tax + " руб")
-            monthExpenses += transactionSum
-            dayExpenses += transactionSum
         } else {
             println("Перевод не может быть произведен, поскольку привышен лимит")
         }
-    } else if (card == "Visa") {
-        if (transactionSum < dayLimit && (monthExpenses + transactionSum) < monthLimit &&
-            (dayExpenses + transactionSum) < dayLimit
-        ) {
-            tax = (transactionSum * 0.075).toInt()
+    } else if (card == "Mastercard" && (allMonthTransactions + transactionSum) > masterCardLimit
+        && (allMonthTransactions + transactionSum) < monthLimit
+    ) {
+        if (transactionSum < dayLimit) {
+            tax = (((allMonthTransactions + transactionSum) - masterCardLimit) * 0.006).toInt() + 20
+            println("Ваша комиссия состаляет " + tax + " руб")
+        } else {
+            println("Перевод не может быть произведен, поскольку привышен лимит")
+        }
+    } else if (card == "Visa" && (allMonthTransactions + transactionSum) < monthLimit) {
+        if (transactionSum < dayLimit) {
+            tax = (transactionSum * 0.0075).toInt()
             if (tax < 35) {
                 tax = 35
             }
             println("Ваша комиссия состаляет " + tax + " руб")
-            monthExpenses += transactionSum
-            dayExpenses += transactionSum
         } else {
             println("Перевод не может быть произведен, поскольку привышен лимит")
         }
